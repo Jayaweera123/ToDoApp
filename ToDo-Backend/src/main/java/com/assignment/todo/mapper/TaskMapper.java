67d719c9b3b2c91f99d6075c6b9b0dece933c47f
Jayaweera123 <155569167+Jayaweera123@users.noreply.github.com>
2025-02-16 09:05:43 +0530

    mapper

---
+++ ToDo-Backend/src/main/java/com/assignment/todo/mapper/TaskMapper.java
@@ -0,0 +1,38 @@
+package com.assignment.todo.mapper;
+
+import com.assignment.todo.dto.TaskDto;
+import com.assignment.todo.entity.Task;
+
+public class TaskMapper {
+
+    // Converts a Task entity to a TaskDto
+    public static TaskDto mapToTaskDto(Task task) {
+        if (task == null) {
+            return null;  // Handle null task: return null DTO if the task is null
+        }
+        return new TaskDto(
+                task.getId(), 
+                task.getTittle(),
+                task.getDescription(),
+                task.getCreated(),
+                task.getUpdated(),
+                task.isCompleted()
+        );
+    }
+
+    public static Task mapToTask(TaskDto taskDto) {
+        if (taskDto == null) {
+            return null;  // Handle null taskDto
+        }
+        return new Task(
+                taskDto.getId(),
+                taskDto.getTitle(),
+                taskDto.getDescription(),
+                taskDto.getCreatedAt(),
+                taskDto.getUpdatedAt(),
+                taskDto.isCompleted()
+
+
+        );
+    }
+}
